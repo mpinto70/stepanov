d09126ffbac0a27f4d7af8bbbe7b513ed5e3ff0e
Marcelo Pinto <mpinto70@gmail.com>
2017-12-23 18:07:45 -0500

    add lemma

--- test/EofP/chapter_02/TransformationsTest.cpp
+++ test/EofP/chapter_02/TransformationsTest.cpp
@@ -2,12 +2,17 @@
 
 #include <gtest/gtest.h>
 
+#include <functional>
+#include <random>
+
 namespace EofP {
 namespace chapter_02 {
 
 TEST(TransformationTest, abs) {
     EXPECT_EQ(abs(2.3), 2.3);
     EXPECT_EQ(abs(-2.3), 2.3);
+    EXPECT_EQ(abs(78), 78);
+    EXPECT_EQ(abs(-78), 78);
 }
 
 TEST(TransformationTest, euclidean_norm_2) {
@@ -16,6 +21,31 @@ TEST(TransformationTest, euclidean_norm_2) {
 
 TEST(TransformationTest, euclidean_norm_3) {
     EXPECT_EQ(euclidean_norm(3.0, 4.0, 5.0), std::sqrt(50.0));
+}
+
+namespace {
+double generate() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<> dis(-1000.0, 1000.0);
+    return dis(gen);
+}
+}
+
+TEST(TransformationTest, lemma_2_1) {
+    for (size_t i = 0; i < 10000; ++i) {
+        const auto x = generate();
+        const auto y = generate();
+        const auto z = generate();
+        const auto e_xyz = euclidean_norm(x, y, z);
+        const auto e_xy = euclidean_norm(x, y);
+        const auto e_xz = euclidean_norm(x, z);
+        const auto e_yz = euclidean_norm(y, z);
+        const std::string msg = std::to_string(x) + " / " + std::to_string(y) + " / " + std::to_string(z);
+        EXPECT_DOUBLE_EQ(e_xyz, euclidean_norm(e_xy, z)) << msg;
+        EXPECT_DOUBLE_EQ(e_xyz, euclidean_norm(e_xz, y)) << msg;
+        EXPECT_DOUBLE_EQ(e_xyz, euclidean_norm(e_yz, x)) << msg;
+    }
 }
 
 }
