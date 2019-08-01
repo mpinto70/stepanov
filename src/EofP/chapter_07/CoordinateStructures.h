c548075eb42b88a2b0d9fc28430ab6ff6341fe7e
Marcelo Pinto <mpinto70@gmail.com>
2019-07-31 23:12:59 -0400

    start working on chapter 7

---
+++ src/EofP/chapter_07/CoordinateStructures.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iterator>
+#include <memory>
+
+namespace EofP {
+
+// Section 7.1
+
+template <typename T>
+struct BifurcateCoordinate;
+
+template <typename T>
+struct BinaryNode {
+    using Type = T;
+    explicit BinaryNode(T t)
+          : value(std::move(t)) {}
+    T& operator*() {
+        return value;
+    }
+    const T& operator*() const {
+        return value;
+    }
+
+private:
+    T value;
+    std::unique_ptr<BinaryNode<T>> left;
+    std::unique_ptr<BinaryNode<T>> right;
+
+    friend class BifurcateCoordinate<BinaryNode<T>>;
+};
+
+template <typename Node>
+struct BifurcateCoordinate {
+    static bool empty(const Node& node) {
+        return not has_left_successor(node) && not has_right_successor(node);
+    }
+    static bool has_left_successor(const Node& node) {
+        return node.left != nullptr;
+    }
+    static bool has_right_successor(const Node& node) {
+        return node.right != nullptr;
+    }
+    static const Node& left_successor(const Node& node) {
+        return *node.left;
+    }
+    static const Node& right_successor(const Node& node) {
+        return *node.right;
+    }
+    static Node& left_successor(Node& node) {
+        return *node.left;
+    }
+    static Node& right_successor(Node& node) {
+        return *node.right;
+    }
+    static void add_left_successor(Node& node, typename Node::Type value) {
+        node.left = std::make_unique<Node>(std::move(value));
+    }
+    static void add_right_successor(Node& node, typename Node::Type value) {
+        node.right = std::make_unique<Node>(std::move(value));
+    }
+};
+}
---
+++ test/EofP/chapter_07/CoordinateStructuresTest.cpp
@@ -0,0 +1,63 @@
+#include "EofP/chapter_06/Iterators.h"
+
+#include <gtest/gtest.h>
+
+#include <EofP/chapter_07/CoordinateStructures.h>
+#include <vector>
+
+namespace EofP {
+
+TEST(CoordinateStructuresTest, build_empty_tree_string) {
+    using bc = BifurcateCoordinate<BinaryNode<std::string>>;
+    const BinaryNode<std::string> root("root string");
+
+    EXPECT_TRUE(bc::empty(root));
+    EXPECT_FALSE(bc::has_left_successor(root));
+    EXPECT_FALSE(bc::has_right_successor(root));
+
+    EXPECT_EQ(*root, "root string");
+}
+
+TEST(CoordinateStructuresTest, build_empty_tree_int) {
+    using bc = BifurcateCoordinate<BinaryNode<int>>;
+    const BinaryNode<int> root(1);
+
+    EXPECT_TRUE(bc::empty(root));
+    EXPECT_FALSE(bc::has_left_successor(root));
+    EXPECT_FALSE(bc::has_right_successor(root));
+
+    EXPECT_EQ(*root, 1);
+}
+
+TEST(CoordinateStructuresTest, build_tree_1_level_string) {
+    using bc = BifurcateCoordinate<BinaryNode<std::string>>;
+    BinaryNode<std::string> root("root string");
+
+    bc::add_left_successor(root, "left 1");
+    bc::add_right_successor(root, "right 1");
+
+    EXPECT_FALSE(bc::empty(root));
+    EXPECT_TRUE(bc::has_left_successor(root));
+    EXPECT_TRUE(bc::has_right_successor(root));
+
+    EXPECT_EQ(*root, "root string");
+    EXPECT_EQ(*bc::left_successor(root), "left 1");
+    EXPECT_EQ(*bc::right_successor(root), "right 1");
+}
+
+TEST(CoordinateStructuresTest, build_tree_1_level_int) {
+    using bc = BifurcateCoordinate<BinaryNode<int>>;
+    BinaryNode<int> root(1);
+
+    bc::add_left_successor(root, 2);
+    bc::add_right_successor(root, 3);
+
+    EXPECT_FALSE(bc::empty(root));
+    EXPECT_TRUE(bc::has_left_successor(root));
+    EXPECT_TRUE(bc::has_right_successor(root));
+
+    EXPECT_EQ(*root, 1);
+    EXPECT_EQ(*bc::left_successor(root), 2);
+    EXPECT_EQ(*bc::right_successor(root), 3);
+}
+}
